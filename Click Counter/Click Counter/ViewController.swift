053b6d89cd94f39b5e4e2a1caeeb7610d30b065d
ErichClark <erich.clark@gmail.com>
2018-03-06 20:13:38 -0500

    Added button ad label to view with code

--- Click Counter/Click Counter/ViewController.swift
+++ Click Counter/Click Counter/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // label
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        view.addSubview(label)
+        
+        // button
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y: 250, width: 80, height: 60)
+        button.setTitle("Click Me", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
     }
 
 }
