9ab802512c107938227ac7129664786b22383426
ErichClark <erich.clark@gmail.com>
2018-03-06 19:43:40 -0500

    First commit

---
+++ Click Counter/Click Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Erich Clark on 3/6/18.
+//  Copyright © 2018 Erich Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
